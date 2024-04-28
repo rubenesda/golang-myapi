1fb837c4e3b9cbbb293a6ff04bec79aacdc1d3a0
Ruben Dario Escandon <rescandon@vovsolutions.com>
2024-04-28 15:25:48 -0500

    edit(http-server): add a prometheus-client to current http-server and add new endpoints

--- main.go
+++ main.go
@@ -3,9 +3,33 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"io"
 	"net/http"
 	"os"
+)
+
+var (
+	appVersion string
+	version = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "version",
+		Help: "Version information about this binary",
+		ConstLabels: map[string]string{
+			"version": appVersion,
+		},
+	})
+
+	httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "The count of all HTTP requests",
+	}, []string{"code","method"})
+
+	httpRequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "http_request_duration_seconds",
+		Help: "Duration of all HTTP requests",
+	}, []string{"code", "handler", "method"})
+
 )
 
 func handleStatus(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +39,36 @@ func handleStatus(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := ":8082"
 
+	r := prometheus.NewRegistry()
+	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(httpRequestDuration)
+	r.MustRegister(version)
+
+	fmt.Printf("listening on port %s\n", port)
+
+	foundHandler := http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello!"))
+	})
+
+	foundChain := promhttp.InstrumentHandlerDuration(
+		httpRequestDuration.MustCurryWith(prometheus.Labels{"handler": "found"}),
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, foundHandler),
+	)
+
+	notfoundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
+
+	internalErrorHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
 	http.HandleFunc("/status", handleStatus)
+	http.HandleFunc("/", foundChain)
+	http.Handle("/err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, notfoundHandler))
+	http.Handle("/internal-err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, internalErrorHandler))
+	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 
 	err := http.ListenAndServe(port, nil)
 
